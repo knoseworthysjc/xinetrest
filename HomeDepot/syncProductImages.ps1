e535710c4e58d9c8c3d461676161d37a8a2ab7e2
Kevin <kevin.noseworthy@stjoseph.com>
2023-03-16 12:51:34 -0400

    change time to hours

--- HomeDepot/syncProductImages.ps1
+++ HomeDepot/syncProductImages.ps1
@@ -3,7 +3,7 @@ $url = "https://sjccontent.sharepoint.com/teams/$team"
 $conn = Connect-PnPOnline -Tenant sjccontent.onmicrosoft.com -ClientId "$Env:sharepointclientid" -Thumbprint "$Env:sharepointthumbprint"  -url $url
 
 $inFolder = "\\10.136.209.199\HomeDepot\Master Assets\Product_Images\*.*"
-$upperBound = [DateTime]::Now.Subtract([TimeSpan]::FromDays(1))
+$upperBound = [DateTime]::Now.Subtract([TimeSpan]::FromHours(1))
 #$files = Get-ChildItem -Path $inFolder -Filter "*.tif" | Where { ! $_.PSIsContainer -and $_.LastWriteTime -gt $upperBound} | Select Name,FullName,LastWriteTime
 $files = Get-ChildItem -Path $inFolder -Include "*.tif","*.jpg" | Where { ! $_.PSIsContainer -and $_.LastWriteTime -gt $upperBound} | Select Name,FullName,LastWriteTime
 
