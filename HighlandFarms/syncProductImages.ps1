05a2dfa9cd6823e461d13ddc1fac8e4f84c6fdaa
Kevin <kevin.noseworthy@stjoseph.com>
2023-03-24 14:04:12 -0400

    sync product images and added hlf sync assets

--- HighlandFarms/syncProductImages.ps1
+++ HighlandFarms/syncProductImages.ps1
@@ -1,18 +1,23 @@
-$team = "HomeDepotContent"
+$team = "HighlandFarmsContent"
 $url = "https://sjccontent.sharepoint.com/teams/$team"
 $conn = Connect-PnPOnline -Tenant sjccontent.onmicrosoft.com -ClientId "$Env:sharepointclientid" -Thumbprint "$Env:sharepointthumbprint"  -url $url
 
 function uploadSharepoint($inFolder, $spFolder, $inc)
 {
-        $upperBound = [DateTime]::Now.Subtract([TimeSpan]::FromHours(1))
-        $files = Get-ChildItem -Path $inFolder -Include $inc | Where { ! $_.PSIsContainer -and $_.LastWriteTime -gt $upperBound} | Select Name,FullName,LastWriteTime
+    
+        $upperBound = [DateTime]::Now.Subtract([TimeSpan]::FromDays(10))
+        $files = Get-ChildItem -Path $inFolder -Include $inc -recurse | Where { ! $_.PSIsContainer -and $_.LastWriteTime -gt $upperBound} | Select Name,FullName,LastWriteTime
         write-Host("Total Files:" + $files.length)
-        foreach($file in $files)
+       foreach($file in $files)
         {        
         $n = $file.Name
         $fn = $file.FullName
-        $n
-        Add-PnPFile -Path "$fn" -Folder $spFolder -Values @{Title="$n";local_path="$fn";asset_status="New"}        
+        
+        $category = $inFolder.split("\") | Select-Object -Last 2 | Select-Object -First 1
+        $sp = "Product Images\$category"
+        $values = @{Title="$n";local_path="$fn";product_category="$category"}
+        
+        Add-PnPFile -Path "$fn" -Folder "$sp" -Values $values       
         }
 }
 <#
@@ -30,7 +35,15 @@ foreach($file in $files)
 
     }
     #>
-$inc = "*.tif","*.jpg"
-$incb = "*.ai","*.tif"
-uploadSharepoint "\\10.136.209.199\HomeDepot\Master Assets\Product_Images\*.*" "product_images" $inc
-uploadSharepoint "\\10.136.209.199\HomeDepot\Master Assets\Logos\*.*" "Logos" $incb
+$inc = "*.tif","*.psd","*.jpg"
+
+$paths = Get-ChildItem "\\10.136.209.199\Highland_Farms\Highland_Farms_Assets\" 
+foreach($f in $paths)
+{
+    $f.Name
+    $f.FullName
+    $pth = $f.FullName
+    
+    uploadSharepoint "$pth\*.*" "Product Images" $inc
+}
+
